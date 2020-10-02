0c33dbbcbe7a28e8cb1bd8762428b2447c026cc4
ememeti <endrit.memeti@mavs.uta.edu>
2020-10-01 21:57:33 -0500

    removed unused folders and documented test code

--- maptest/map_test.py
+++ maptest/map_test.py
@@ -1,16 +1,28 @@
+# Library that gives us the functions to create
+#   the map
 import plotly.io as pio
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
 
+# Reads csv files and sorts the states by their
+#   fips codes
+import pandas as pd
+
+# Object that will be used to execute the 
+#   map creation features 
+import plotly.express as px
+
+# Reads the map data from a website to help
+#   generate the map
 from urllib.request import urlopen
 import json
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
     counties = json.load(response)
 
+# Reads in the info from our COVID data set
 df = pd.read_csv("./cases/Cases-4-7-20vs4-8-20-intDays7/ALL-CommID-Cases-4-7-20vs4-8-20-intDays7.csv",
                     dtype={"fips": str}, encoding='latin-1')
 
+# Creates the map object and fills it with 
+#   information
 fig = px.choropleth(df, geojson=counties, locations='fips', color='CommunityID',
                            color_continuous_scale=px.colors.diverging.RdYlGn[::-1],
                            range_color=(0, 7),
@@ -25,12 +37,16 @@ fig = px.choropleth(df, geojson=counties, locations='fips', color='CommunityID',
                                          "Percent_of_adults_with_a_high_school_diploma_only_2014-18"] 
                           )
 
+# Fixes the origin to the right of the map
+#   information
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0},
                      coloraxis_colorbar=dict( title = "Percentage Change in the Number of New Cases",
                      ticks = 'inside',
                      tickvals = [0, 1, 2, 3, 4, 5, 6, 7, 8],
                      ticktext=['[-100%]', '(-100%, -50%]', '(-50%,0%)', '[0%]', '(0%, 50%]', '(50%, 100%]', '(100%, 200%]', 'More than 200%']))
+
+# Displays the figure when program is executed    
 fig.show()
 
-
+# Creates an html file of the map
 pio.write_html(fig, file='map.html', auto_open=False)
