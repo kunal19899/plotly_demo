d318e341d18aee47e950d85993669cb299a3915b
Kunal Samant <kunal19899@gmail.com>
2020-10-29 23:35:30 -0500

    working to find wtform/javascript

--- routes.py
+++ routes.py
@@ -19,28 +19,37 @@ def index():
 @app.route("/map", methods=['GET','POST'])
 def map():
     correctInput = False
-    inputForm = InputForm()
+    inputForm = InputForm(request.form)
     message = ''
 
     if request.method == "POST":
         ipt = request.form
         periodLength = ipt['periodLength']
         if periodLength == "Select a Period Length": periodLength = -1
-
-
         start_of_startDate = ipt['start_of_startDate']
-        start_of_startDate = datetime.strptime(start_of_startDate, '%Y-%m-%d')
-        start_of_endDate = start_of_startDate.date() + timedelta(days=int(periodLength))
-
-
         start_of_endDate = ipt['start_of_endDate']
-        start_of_endDate = datetime.strptime(start_of_endDate, '%Y-%m-%d')
-        end_of_endDate = start_of_endDate.date() + timedelta(days=int(periodLength))
         interval = ipt['interval']
         if interval == "% Interval": interval = -1
+
         
-        if (periodLength != -1) and (start_of_startDate != '') and (start_of_endDate != '') and (interval != -1):
-            correctInput = True
+        if (periodLength == -1) or (start_of_startDate == '') or (start_of_endDate == '') or (interval == -1):
+            message = "All Fields must be non-empty!"
+            return render_template("index.html", form = inputForm, message = message)
+
+        start_of_startDate = datetime.strptime(start_of_startDate, '%Y-%m-%d')
+        end_of_startDate = start_of_startDate.date() + timedelta(days=int(periodLength))
+
+        start_of_endDate = datetime.strptime(start_of_endDate, '%Y-%m-%d')
+        end_of_endDate = start_of_endDate.date() + timedelta(days=int(periodLength))
+
+
+        print(start_of_endDate)
+        print(end_of_startDate)
+        print(end_of_startDate < start_of_endDate.date())
+
+        if end_of_startDate > start_of_endDate.date():
+            message = 'Periods cannot overlap!'
+            return render_template("index.html", form = inputForm, message = message)
             
             ###########TODO: create sync with Endrit's code##################
             
@@ -48,13 +57,7 @@ def map():
 
 
             ##################################################################
-    if not correctInput:
-        message = "All Fields must be non-empty!"
-
-        return render_template("index.html", form = inputForm, message = message)
-
-    # call function for plotly here - returns 
-    
+        
 
     return render_template("index.html", form = inputForm, message = message)
 
